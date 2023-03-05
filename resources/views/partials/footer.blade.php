c92fdf83632fedaea6544b65133352ed40b40fc6
alimasha-design <alimashadesign@gmail.com>
2023-03-05 10:46:54 +0600

    Updated compare design

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,9 +3,9 @@
     <div class="row">
         <div class="footer-content col text-right">
             @foreach (App\Page::all() as $page)
-                <a href="/p-{{ $page->page_slug }}">{{ $page->page_title }}</a> |
+                <a href="/p-{{ $page->page_slug }}">{{ $page->page_title }}</a>
             @endforeach
-            <a href="{{ route('contact') }}">{{ __('Get In Touch') }}</a> |
+            <a href="{{ route('contact') }}">{{ __('Get In Touch') }}</a>
             <a href="{{ route('sitemap') }}">{{ __('Sitemap') }}</a>
         </div><!-- /.pull-right -->
     </div><!-- /.row -->
